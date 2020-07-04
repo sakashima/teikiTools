133b5cf9e37831a924939ccf6caa42d4fafc063b
sakashima <saka4emi@gmail.com>
2020-07-04 20:09:02 +0900

    イバラの戦闘行動画面で『自分○％』の実数を表示するスクリプト

--- IBRhpspPercentViewer.user.js
+++ IBRhpspPercentViewer.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         IBRhpspPercentViewer
-// @namespace    http://tampermonkey.net/
+// @namespace    https://twitter.com/nrsr_cw
 // @version      0.5
+// @downloadURL  https://github.com/sakashima/teikiTools/blob/master/IBRhpspPercentViewer.user.js
+// @updateURL    https://github.com/sakashima/teikiTools/blob/master/IBRhpspPercentViewer.user.js
 // @description  イバラの戦闘行動画面で『自分ＨＰ○％以下』『自分ＳＰ○％以上・以下』の実数を表示するスクリプト
-// @author       nrsr_cw
+// @author       のりしろ nrsr_cw
 // @match        http://lisge.com/ib/act_battle.php
 // @grant        none
 
